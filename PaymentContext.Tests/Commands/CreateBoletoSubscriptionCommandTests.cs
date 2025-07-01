dde960895fb5e37e466d001c5c4030ef34f6d333
Diosney Gomes <diosneyf.gomes@gmail.com>
2025-06-30 22:14:39 -0300

    Aula - Testando os commands

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValuesObjects;
+
+namespace PaymentContext.Tests.Commands
+{
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [Fact]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+
+            command.FirstName = "";
+            
+            command.Validate();
+
+            Assert.Equal(false, command.IsValid);
+        }
+    }
+}
